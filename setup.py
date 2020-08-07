4a90961eb752b8b0d868963d74f179f21f322485
Jeremy Axmacher <jaxmacher@presidio.com>
2020-08-07 17:49:33 -0400

    Add requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
+import json
+
 from setuptools import setup
+
+install_requires = []
+tests_require = []
+
+with open("Pipfile.lock") as fd:
+    lock_data = json.load(fd)
+    install_requires = [
+        package_name + package_data["version"]
+        for package_name, package_data in lock_data["default"].items()
+    ]
+    tests_require = [
+        package_name + package_data["version"]
+        for package_name, package_data in lock_data["develop"].items()
+    ]
 
 setup(
     name="cfn_mod",
-    version="0.1",
+    version="0.1.2",
     description="CloudFormation Modules Helper",
     url="http://github.com/storborg/funniest",
     author="Jeremy Axmacher",
@@ -10,5 +26,7 @@ setup(
     license="MIT",
     packages=["cfn_mod"],
     zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
     entry_points={"console_scripts": ["cfn-mod=cfn_mod.command_line:main"]},
 )
