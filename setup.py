ce0ea2ecc4c0efeebc7a173cb2767c015aa59ffd
Jeremy Axmacher <jaxmacher@presidio.com>
2020-08-07 17:42:12 -0400

    Fix setup

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup
 
-setup(entry_points={"console_scripts": ["cfn-mod=cfn_mod.command_line:main"]})
+setup(
+    name="cfn_mod",
+    version="0.1",
+    description="CloudFormation Modules Helper",
+    url="http://github.com/storborg/funniest",
+    author="Jeremy Axmacher",
+    author_email="jaxmacher@presidio.com",
+    license="MIT",
+    packages=["cfn_mod"],
+    zip_safe=False,
+    entry_points={"console_scripts": ["cfn-mod=cfn_mod.command_line:main"]},
+)
