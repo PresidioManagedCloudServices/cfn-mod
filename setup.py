1aca9c08b007749c2d7ce8ba64efcf88a943a6be
Jeremy Axmacher <jaxmacher@presidio.com>
2020-09-25 11:42:15 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("Pipfile.lock") as fd:
 
 setup(
     name="cfn-mod",
-    version="0.1.11",
+    version="0.1.12",
     description="CloudFormation Modules Helper",
     url="https://github.com/PresidioManagedCloudServices/cfn-mod",
     author="Jeremy Axmacher",
